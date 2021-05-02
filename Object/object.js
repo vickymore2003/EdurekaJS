ce6968d861ad8717241d0b0aaf9524b62ee503c0
vickymore2003 <vickymore2003@gmail.com>
2021-05-03 00:56:03 +0530

    Object types

--- Object/object.js
+++ Object/object.js
@@ -1,20 +1,49 @@
+
+//Type one for creating an object
 function createPerson() {
     var person = new Object();
-
     person.name = "Sunny";
     person.age = 31;
-    person.phno = 1236641;
+    person.phno = 1111111;
     return person;
 }
+var sunny1 = createPerson();
 
-var sunny = createPerson();
-
+//Type two for creating an object
 function createPerson2() {
     var person = {};
-
-    person.name = "Sunny3";
-    person.age = 31;
-    person.phno = 1236641;
+    person.name = "Sunny2";
+    person.age = 32;
+    person.phno = 2222222;
     return person;
 }
- sunny = createPerson2();
+sunny2 = createPerson2();
+
+//Type three for creating an object
+function createPerson3() {
+    var person = {};
+    person['name'] = "Sunny3";
+    person['age'] = 33;
+    person['phno'] = 33333333;
+    return person;
+}
+sunny3 = createPerson3();
+
+//Type four for creating an object
+function createPerson4() {
+    var person = {
+        name: "Sunny4",
+        age: 34,
+        designation: "QA",
+        phno: 444444
+    };
+    return person;
+}
+sunny4 = createPerson4()
+
+// alert("name is " + sunny1.name + ", age is "
+//     + sunny2.age + " and contact number  "
+//     + sunny3.phno + " his desg is " + sunny4.designation)
+
+
+
